9d7a4c783184be5886e4790912c2b045051077cf
IsaacPrice <iprice25250@gmail.com>
2024-01-29 19:05:41 -0500

    flappy bird project is functional

--- flappy-Bird/script.js
+++ flappy-Bird/script.js
@@ -7,7 +7,7 @@ var Pipe = /** @class */ (function () {
         var clientHeight = window.innerHeight;
         var Difference = clientHeight * pipeVariance; // the center of the pipes is within this
         var Center = Math.random() * Difference; // get a random float 0-1 and multiply it by the difference
-        Center += ((clientHeight * (1 - pipeVariance)) / 2); // Add padding to the center
+        //Center += ((clientHeight * (1 - pipeVariance)) / 2); // Add padding to the center
         // Create top boundary
         this.TopBound = document.createElement("div");
         this.TopBound.classList.add("pipe-top");
@@ -15,7 +15,6 @@ var Pipe = /** @class */ (function () {
         this.TopBound.style.height = "".concat(clientHeight - (Center + (clientHeight * pipeSpace / 2)), "px");
         this.TopBound.style.top = "0px";
         this.TopBound.style.right = "-100px";
-        this.TopBound.style.transition = "all linear ".concat(1000 / fps, "ms");
         this.TopBound.id = "".concat(this.id);
         // Create the botom boundary
         this.BottomBound = document.createElement("div");
@@ -24,11 +23,7 @@ var Pipe = /** @class */ (function () {
         this.BottomBound.style.height = "".concat(Center - (pipeSpace / 2), "px");
         this.BottomBound.style.bottom = "0px";
         this.BottomBound.style.right = "-100px";
-        this.BottomBound.style.transition = "all linear ".concat(1000 / fps, "ms");
         this.BottomBound.id = "-".concat(this.id);
-        // debug stuff
-        console.log(this.BottomBound.style.height);
-        console.log(this.BottomBound.style.height);
         // Add the elements to the document
         document.querySelectorAll(".game")[0].appendChild(this.TopBound);
         document.querySelectorAll(".game")[0].appendChild(this.BottomBound);
@@ -51,40 +46,42 @@ var Pipe = /** @class */ (function () {
     return Pipe;
 }());
 var Bird = /** @class */ (function () {
-    function Bird(flapStrength, gravity, speed, startingPosition, fps) {
-        this.FlapStrength = flapStrength;
-        this.Gravity = gravity;
-        this.Speed = speed;
-        this.Position = startingPosition;
-        this.Height = document.body.clientHeight / 10;
-        this.Width = this.Height;
+    function Bird(flapStrength, gravity, maxVelocity, speed, startingPosition, fps) {
         this.Fps = fps;
+        this.Velocity = 0;
+        this.Speed = speed;
+        this.IsPlaying = true;
+        this.Gravity = gravity;
+        this.Width = this.Height;
+        this.MaxVeloctiy = maxVelocity;
+        this.FlapStrength = flapStrength;
+        this.Height = window.innerHeight / 15;
+        this.Position = [window.innerWidth * startingPosition[0], window.innerHeight * startingPosition[1]];
         // Create the html element of the flappy bird
         var temp = document.createElement("div");
         temp.classList.add("bird");
+        temp.id = "bird";
+        temp.style.position = "absolute";
+        temp.style.top = "".concat(window.innerHeight - this.Position[1], "px");
+        temp.style.left = "".concat(this.Position[0], "px");
         temp.style.height = "".concat(this.Height, "px");
         temp.style.width = "".concat(this.Width, "px");
-        temp.id = "bird";
-        temp.style.transition = "all linear ".concat(1000 / fps, "ms");
-        temp.style.bottom = "".concat(startingPosition[1] * document.body.clientHeight, "px");
-        temp.style.left = "".concat(startingPosition[0] * document.body.clientWidth, "px");
-        this.htmlElement = temp;
         // Add the bird to the document
-        document.querySelectorAll(".game")[0].appendChild(this.htmlElement);
+        document.querySelectorAll(".game")[0].appendChild(temp);
     }
-    // This takes all of the pipes, and also if the user jumped
+    // Takes all of the pipes, and also if the user jumped
     // Returns 1 for getting a point, 0 if nothing happend, and -1 if the bird crashes
     Bird.prototype.Update = function (pipes) {
         var _this = this;
         // Update position
-        this.Velocity -= (1000 / this.Fps) * this.Gravity;
+        this.Velocity = (Math.abs(this.Velocity) < this.MaxVeloctiy) ? this.Velocity - ((1 / this.Fps) * this.Gravity) : -1 * this.MaxVeloctiy;
         this.Position[1] += this.Velocity;
         var birdy = document.getElementById("bird");
         if (birdy !== null) {
-            birdy.style.bottom = "".concat(this.Position[1], "px");
+            birdy.style.top = "".concat(window.innerHeight - this.Position[1], "px");
         }
         // Check for collisions with both the ground and pipes, along with if the user has earned a point
-        var distance = this.Speed / 2;
+        var distance = this.Speed * 5;
         var tempPoints = 0;
         pipes.forEach(function (element) {
             if (_this.CheckCollision(element.BottomBound) || _this.CheckCollision(element.TopBound)) {
@@ -106,55 +103,67 @@ var Bird = /** @class */ (function () {
     // Function to check if two objects are colliding
     Bird.prototype.CheckCollision = function (object) {
         // Get the object borders
-        var rect1 = document.querySelectorAll(".bird")[0].getBoundingClientRect();
+        var obj = document.getElementById("bird");
         var rect2 = object.getBoundingClientRect();
         // Check if they are colliding
-        return !(rect2.left > rect1.right ||
-            rect2.right < rect1.left ||
-            rect2.top > rect1.bottom ||
-            rect2.bottom < rect1.top);
+        if (obj !== null) {
+            var rect1 = obj.getBoundingClientRect();
+            return !(rect2.left > rect1.right ||
+                rect2.right < rect1.left ||
+                rect2.top > rect1.bottom ||
+                rect2.bottom < rect1.top);
+        }
+        else {
+            return false;
+        }
     };
     Bird.prototype.Xdistance = function (object) {
         // Get the object borders
-        var rect1 = document.querySelectorAll("bird")[0].getBoundingClientRect();
+        var obj = document.getElementById("bird");
         var rect2 = object.getBoundingClientRect();
         // Return the difference of the x values
-        var center1 = (rect1.left + rect1.right) / 2;
-        var center2 = (rect2.left + rect2.right) / 2;
-        return Math.abs(center1 - center2);
+        if (obj !== null) {
+            var rect1 = obj.getBoundingClientRect();
+            var center1 = (rect1.left + rect1.right) / 2;
+            var center2 = (rect2.left + rect2.right) / 2;
+            return Math.abs(center1 - center2);
+        }
+        else {
+            return -1;
+        }
     };
     Bird.prototype.jump = function () {
-        this.Velocity += this.FlapStrength;
+        this.Velocity = this.FlapStrength;
     };
     return Bird;
 }());
 var Game = /** @class */ (function () {
-    function Game(speed, spawnRate, pipeVariance, pipeSpace, flapStrength, gravity, birdStartPercentage, fps) {
+    function Game(speed, spawnRate, pipeVariance, pipeSpace, flapStrength, gravity, maxVelocity, birdStartPercentage, fps) {
         if (speed === void 0) { speed = .75; }
-        if (spawnRate === void 0) { spawnRate = 1; }
+        if (spawnRate === void 0) { spawnRate = 100; }
         if (pipeVariance === void 0) { pipeVariance = .75; }
-        if (pipeSpace === void 0) { pipeSpace = .50; }
-        if (flapStrength === void 0) { flapStrength = 25; }
-        if (gravity === void 0) { gravity = 9.8; }
+        if (pipeSpace === void 0) { pipeSpace = .5; }
+        if (flapStrength === void 0) { flapStrength = 7.5; }
+        if (gravity === void 0) { gravity = 30; }
+        if (maxVelocity === void 0) { maxVelocity = 30; }
         if (birdStartPercentage === void 0) { birdStartPercentage = [.15, .50]; }
-        if (fps === void 0) { fps = 60; }
-        this.Speed = speed;
-        this.SpawnRate = spawnRate;
-        this.PipeVariance = pipeVariance;
-        this.PipeSpace = pipeSpace;
-        this.Gravity = gravity;
+        if (fps === void 0) { fps = 120; }
         this.Score = 0;
         this.Fps = fps;
-        this.ElaspedFrames = 0;
+        this.Speed = speed;
         this.TotalPipes = 1;
+        this.ElaspedFrames = 0;
+        this.SpawnRate = spawnRate;
+        this.PipeSpace = pipeSpace;
+        this.PipeVariance = pipeVariance;
         // Create the flappy bird, using flapStength, gravity, and birdStartPercentage
-        this.FlappyBird = new Bird(flapStrength, gravity, speed, birdStartPercentage, fps);
+        this.FlappyBird = new Bird(flapStrength, gravity, maxVelocity, speed, birdStartPercentage, fps);
         // Create the first pipe
         this.Pipes = Array();
-        var startingPipe = new Pipe(speed, pipeVariance, pipeSpace, fps, 0);
-        this.Pipes.push(startingPipe);
+        //const startingPipe = new Pipe(speed, pipeVariance, pipeSpace, fps, 0);
+        //this.Pipes.push(startingPipe);
         // Setup the inputs
-        var jumper = this.FlappyBird.jump;
+        var jumper = this.FlappyBird.jump.bind(this.FlappyBird);
         window.addEventListener('keydown', jumper);
         // Begin game loop
         this.GameLoop(fps);
@@ -163,16 +172,40 @@ var Game = /** @class */ (function () {
     Game.prototype.GameLoop = function (fps) {
         var _this = this;
         var interval = 1000 / fps;
+        var running = true;
         setInterval(function () {
-            _this.update();
+            if (running) {
+                running = _this.update();
+            }
         }, interval);
     };
     // Checks for inputs, moves the pipe, bird and checks for collisions
     Game.prototype.update = function () {
-        this.FlappyBird.Update(this.Pipes);
-        this.Pipes.forEach(function (element) {
-            element.Update();
-        });
+        var result = this.FlappyBird.Update(this.Pipes);
+        if (result !== -1) {
+            // Spawn in a new pipe if its time
+            if (this.ElaspedFrames % this.SpawnRate == 0) {
+                var tempPipe = new Pipe(this.Speed, this.PipeVariance, this.PipeSpace, this.Fps, this.TotalPipes);
+                this.TotalPipes++;
+                this.Pipes.push(tempPipe);
+            }
+            // Move all of the pipes
+            this.Pipes.forEach(function (element) {
+                element.Update();
+            });
+            // Add to the counters
+            this.ElaspedFrames += 1;
+            this.Score += (result === 1) ? 1 : 0;
+            // Update the Score counter
+            var display = document.getElementById("score-display");
+            if (display !== null) {
+                display.textContent = "".concat(this.Score);
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
     };
     return Game;
 }());
--- flappy-Bird/script.ts
+++ flappy-Bird/script.ts
@@ -16,7 +16,7 @@ class Pipe {
         const clientHeight: number = window.innerHeight;
         const Difference = clientHeight * pipeVariance; // the center of the pipes is within this
         let Center = Math.random() * Difference; // get a random float 0-1 and multiply it by the difference
-        Center += ((clientHeight * (1 - pipeVariance)) / 2); // Add padding to the center
+        //Center += ((clientHeight * (1 - pipeVariance)) / 2); // Add padding to the center
 
         // Create top boundary
         this.TopBound = document.createElement("div");
@@ -25,7 +25,6 @@ class Pipe {
         this.TopBound.style.height = `${clientHeight - (Center + (clientHeight * pipeSpace / 2))}px`;
         this.TopBound.style.top = "0px";
         this.TopBound.style.right = "-100px";
-        this.TopBound.style.transition = `all linear ${1000 / fps}ms`;
         this.TopBound.id = `${this.id}`;
 
         // Create the botom boundary
@@ -35,12 +34,7 @@ class Pipe {
         this.BottomBound.style.height = `${Center - (pipeSpace / 2)}px`;
         this.BottomBound.style.bottom = "0px";
         this.BottomBound.style.right = "-100px";
-        this.BottomBound.style.transition = `all linear ${1000 / fps}ms`
         this.BottomBound.id = `-${this.id}`;
-
-        // debug stuff
-        console.log(this.BottomBound.style.height);
-        console.log(this.BottomBound.style.height);
 
         // Add the elements to the document
         document.querySelectorAll(".game")[0].appendChild(this.TopBound);
@@ -68,56 +62,58 @@ class Pipe {
 }
 
 class Bird {
+    private Fps: number;
+    private Speed: number;
     private Gravity: number;
     private Velocity: number;
-    private FlapStrength: number;
-    private Speed: number;
     private IsPlaying: boolean;
-    private Fps: number;
-    private htmlElement: HTMLElement;
+    private MaxVeloctiy: number; 
+    private FlapStrength: number;
 
     public Width: number;
     public Height: number;
     public Position: [number, number];
 
-    constructor(flapStrength: number, gravity: number, speed: number, startingPosition: [number, number], fps: number) {
-        this.FlapStrength = flapStrength;
-        this.Gravity = gravity;
-        this.Speed = speed;
-        this.Position = startingPosition;
-        this.Height = document.body.clientHeight / 10;
-        this.Width = this.Height;
+    constructor(flapStrength: number, gravity: number, maxVelocity: number, speed: number, startingPosition: [number, number], fps: number) {
         this.Fps = fps;
+        this.Velocity = 0;
+        this.Speed = speed;
+        this.IsPlaying = true
+        this.Gravity = gravity;
+        this.Width = this.Height;
+        this.MaxVeloctiy = maxVelocity;
+        this.FlapStrength = flapStrength;
+        this.Height = window.innerHeight / 15;
+        this.Position = [window.innerWidth * startingPosition[0], window.innerHeight * startingPosition[1]];
 
         // Create the html element of the flappy bird
         let temp = document.createElement("div");
         temp.classList.add("bird");
+        temp.id = "bird";
+        temp.style.position = "absolute";
+        temp.style.top = `${window.innerHeight - this.Position[1]}px`;
+        temp.style.left = `${this.Position[0]}px`;
         temp.style.height = `${this.Height}px`;
         temp.style.width = `${this.Width}px`;
-        temp.id = "bird";
-        temp.style.transition = `all linear ${1000 / fps}ms`;
-        temp.style.bottom = `${startingPosition[1] * document.body.clientHeight}px`;
-        temp.style.left = `${startingPosition[0] * document.body.clientWidth}px`;
-        this.htmlElement = temp;
 
         // Add the bird to the document
-        document.querySelectorAll(".game")[0].appendChild(this.htmlElement);
+        document.querySelectorAll(".game")[0].appendChild(temp);
     }
 
-    // This takes all of the pipes, and also if the user jumped
+    // Takes all of the pipes, and also if the user jumped
     // Returns 1 for getting a point, 0 if nothing happend, and -1 if the bird crashes
     public Update(pipes: Array<Pipe>): number {
 
         // Update position
-        this.Velocity -= (1000 / this.Fps) * this.Gravity;
+        this.Velocity = (Math.abs(this.Velocity) < this.MaxVeloctiy) ? this.Velocity - ((1 / this.Fps) * this.Gravity) : -1 * this.MaxVeloctiy;
         this.Position[1] += this.Velocity;
-        let birdy = document.getElementById("bird");
+        const birdy = document.getElementById("bird");
         if (birdy !== null) {
-            birdy.style.bottom = `${this.Position[1]}px`;
+            birdy.style.top = `${window.innerHeight - this.Position[1]}px`;
         }
 
         // Check for collisions with both the ground and pipes, along with if the user has earned a point
-        const distance: number = this.Speed / 2;
+        const distance: number = this.Speed * 5;
         let tempPoints: number = 0;
         pipes.forEach(element => {
             if (this.CheckCollision(element.BottomBound) || this.CheckCollision(element.TopBound)) {
@@ -142,39 +138,49 @@ class Bird {
     // Function to check if two objects are colliding
     private CheckCollision(object: HTMLElement): boolean {
         // Get the object borders
-        const rect1 = document.querySelectorAll(".bird")[0].getBoundingClientRect();
+        const obj = document.getElementById("bird");
         const rect2 = object.getBoundingClientRect();
 
         // Check if they are colliding
-        return !(rect2.left > rect1.right || 
-            rect2.right < rect1.left || 
-            rect2.top > rect1.bottom ||
-            rect2.bottom < rect1.top);
+        if (obj !== null) {
+            const rect1 = obj.getBoundingClientRect();
+            return !(rect2.left > rect1.right || 
+                    rect2.right < rect1.left || 
+                    rect2.top > rect1.bottom ||
+                    rect2.bottom < rect1.top);
+        }
+        else {
+            return false;
+        }
     }  
 
     private Xdistance(object: HTMLElement): number {
         // Get the object borders
-        const rect1 = document.querySelectorAll("bird")[0].getBoundingClientRect();
+        const obj = document.getElementById("bird");
         const rect2 = object.getBoundingClientRect();
 
         // Return the difference of the x values
-        const center1 = (rect1.left + rect1.right) / 2;
-        const center2 = (rect2.left + rect2.right) / 2;
-        return Math.abs(center1 - center2);
+        if (obj !== null) {
+            const rect1 = obj.getBoundingClientRect();
+            const center1 = (rect1.left + rect1.right) / 2;
+            const center2 = (rect2.left + rect2.right) / 2;
+            return Math.abs(center1 - center2);
+        }
+        else {
+            return -1;
+        }
     }
 
     public jump() {
-        this.Velocity += this.FlapStrength;
+        this.Velocity = this.FlapStrength;
     }
 }
-
 
 class Game {
     private Speed: number;
     private SpawnRate: number;
     private PipeVariance: number;
     private PipeSpace: number;
-    private Gravity: number;
     private Score: number;
     private Fps: number;
     private ElaspedFrames: number; 
@@ -184,34 +190,34 @@ class Game {
     private TotalPipes: number;
 
     constructor(speed: number = .75, 
-                spawnRate: number = 1, 
-                pipeVariance: number = .75, 
-                pipeSpace: number = .50,    
-                flapStrength: number = 25, 
-                gravity: number = 9.8, 
+                spawnRate: number = 100, 
+                pipeVariance: number = .75,  
+                pipeSpace: number = .5,    
+                flapStrength: number = 7.5, 
+                gravity: number = 30,
+                maxVelocity: number = 30, 
                 birdStartPercentage: [number, number] = [.15, .50], 
-                fps: number = 60) {
+                fps: number = 120) { 
 
-        this.Speed = speed;
-        this.SpawnRate = spawnRate;
-        this.PipeVariance = pipeVariance;
-        this.PipeSpace = pipeSpace;
-        this.Gravity = gravity;
         this.Score = 0;
         this.Fps = fps;
-        this.ElaspedFrames = 0;
+        this.Speed = speed;
         this.TotalPipes = 1;
+        this.ElaspedFrames = 0;
+        this.SpawnRate = spawnRate;
+        this.PipeSpace = pipeSpace;
+        this.PipeVariance = pipeVariance;
 
         // Create the flappy bird, using flapStength, gravity, and birdStartPercentage
-        this.FlappyBird = new Bird(flapStrength, gravity, speed, birdStartPercentage, fps);
+        this.FlappyBird = new Bird(flapStrength, gravity, maxVelocity, speed, birdStartPercentage, fps);
 
         // Create the first pipe
         this.Pipes = Array<Pipe>();
-        const startingPipe = new Pipe(speed, pipeVariance, pipeSpace, fps, 0);
-        this.Pipes.push(startingPipe);
+        //const startingPipe = new Pipe(speed, pipeVariance, pipeSpace, fps, 0);
+        //this.Pipes.push(startingPipe);
 
         // Setup the inputs
-        const jumper = this.FlappyBird.jump;
+        const jumper = this.FlappyBird.jump.bind(this.FlappyBird);
         window.addEventListener('keydown', jumper);
 
         // Begin game loop
@@ -219,20 +225,50 @@ class Game {
     }
 
     // This calls the tick function, depending on the fps
-    private GameLoop(fps: number) {
+    private GameLoop(fps: number) { 
         const interval = 1000 / fps;
+        let running = true;
 
         setInterval(() => {
-            this.update();
+            if (running) {
+                running = this.update();
+            }
         }, interval);
     }
 
     // Checks for inputs, moves the pipe, bird and checks for collisions
-    private update() {
-        this.FlappyBird.Update(this.Pipes);
-        this.Pipes.forEach(element => {
-            element.Update();
-        });
+    private update(): boolean {
+        let result: number = this.FlappyBird.Update(this.Pipes);
+
+        if (result !== -1) {
+
+            // Spawn in a new pipe if its time
+            if (this.ElaspedFrames % this.SpawnRate == 0) {
+                const tempPipe = new Pipe(this.Speed, this.PipeVariance, this.PipeSpace, this.Fps, this.TotalPipes);
+                this.TotalPipes++;
+                this.Pipes.push(tempPipe);
+            }
+
+            // Move all of the pipes
+            this.Pipes.forEach(element => {
+                element.Update();
+            });
+
+            // Add to the counters
+            this.ElaspedFrames += 1;
+            this.Score += (result === 1) ? 1 : 0;
+
+            // Update the Score counter
+            const display: HTMLElement | null = document.getElementById("score-display");
+            if (display !== null) {
+                display.textContent = `${this.Score}`;
+            }
+
+            return true;   
+        } 
+        else {
+            return false;
+        }
     }
 }
 
